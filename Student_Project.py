3b95b76326d6e8432cac8c604136b41bcfcd98aa
Pratham2415 <84369631+Pratham2415@users.noreply.github.com>
2021-06-12 23:03:53 +0530

    Update Student_Project.py

--- Student_Project.py
+++ Student_Project.py
@@ -5,7 +5,7 @@ area_sq = side ** 2
 area_cube = 6 * (side ** 2)
 vol_cube = side ** 3
 
-print("\nThe perimeter of square is : ", #write the variable name for corresponding, 
+print("\nThe perimeter of square is : ", peri_sq, 
       "\nThe area of square is : ", #write the variable name for corresponding, 
       "\nThe area of cube is : ", #write the variable name for corresponding,
      "\nThe volume of cube is : ", #write the variable name for corresponding)
